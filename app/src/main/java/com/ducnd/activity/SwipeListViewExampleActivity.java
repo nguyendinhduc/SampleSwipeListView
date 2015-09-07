4591504ac843fee8c8803fa9a110e49a9e1fcef2
nguyendinhduc <uet.nguyendinhduc@gmail.com>
2015-09-07 20:46:52 +0700

    Sample Swipe ListView

---
+++ app/src/main/java/com/ducnd/activity/SwipeListViewExampleActivity.java
@@ -0,0 +1,256 @@
+
+package com.ducnd.activity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.AbsListView;
+import android.widget.ListView;
+
+import com.ducnd.adapters.PackageAdapter;
+import com.ducnd.adapters.PackageItem;
+import com.ducnd.dialogs.AboutDialog;
+import com.ducnd.sampleswipelistview.R;
+import com.ducnd.utils.PreferencesManager;
+import com.ducnd.utils.SettingsManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import libs.BaseSwipeListViewListener;
+import libs.SwipeListView;
+
+public class SwipeListViewExampleActivity extends FragmentActivity {
+    private static final String TAG = "SwipeListViewActivity";
+
+    private static final int REQUEST_CODE_SETTINGS = 0;
+    private PackageAdapter adapter;
+    private List<PackageItem> data;
+
+    private SwipeListView swipeListView;
+
+    private ProgressDialog progressDialog;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.swipe_list_view_activity);
+
+        data = new ArrayList<PackageItem>();
+
+        adapter = new PackageAdapter(this, data);
+
+        swipeListView = (SwipeListView) findViewById(R.id.example_lv_list);
+
+        swipeListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            swipeListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+
+                @Override
+                public void onItemCheckedStateChanged(ActionMode mode, int position,
+                                                      long id, boolean checked) {
+                    mode.setTitle("Selected (" + swipeListView.getCountSelected() + ")");
+                }
+
+                @Override
+                public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.menu_delete:
+                            swipeListView.dismissSelected();
+                            mode.finish();
+                            return true;
+                        default:
+                            return false;
+                    }
+                }
+
+                @Override
+                public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                    MenuInflater inflater = mode.getMenuInflater();
+                    inflater.inflate(R.menu.menu_choice_items, menu);
+                    return true;
+                }
+
+                @Override
+                public void onDestroyActionMode(ActionMode mode) {
+                    swipeListView.unselectedChoiceStates();
+                }
+
+                @Override
+                public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                    return false;
+                }
+            });
+        }
+
+        swipeListView.setSwipeListViewListener(new BaseSwipeListViewListener() {
+            @Override
+            public void onOpened(int position, boolean toRight) {
+                Log.i(TAG, "setSwipeListViewListener onOpen");
+            }
+
+            @Override
+            public void onClosed(int position, boolean fromRight) {
+                Log.i(TAG, "setSwipeListViewListener onClosed");
+            }
+
+            @Override
+            public void onListChanged() {
+                Log.i(TAG, "setSwipeListViewListener onListChanged");
+            }
+
+            @Override
+            public void onMove(int position, float x) {
+                Log.i(TAG, "setSwipeListViewListener onMove");
+            }
+
+            @Override
+            public void onStartOpen(int position, int action, boolean right) {
+                Log.i(TAG, String.format("setSwipeListViewListener onStartOpen %d - action %d", position, action));
+            }
+
+            @Override
+            public void onStartClose(int position, boolean right) {
+                Log.i(TAG, String.format("setSwipeListViewListener onStartClose %d", position));
+            }
+
+            @Override
+            public void onClickFrontView(int position) {
+                Log.i(TAG, String.format("setSwipeListViewListener onClickFrontView %d", position));
+            }
+
+            @Override
+            public void onClickBackView(int position) {
+                Log.i(TAG, String.format("setSwipeListViewListener onClickBackView %d", position));
+            }
+
+            @Override
+            public void onDismiss(int[] reverseSortedPositions) {
+                for (int position : reverseSortedPositions) {
+                    data.remove(position);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+        });
+
+        swipeListView.setAdapter(adapter);
+
+        reload();
+
+        new ListAppTask().execute();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+
+    }
+
+    private void reload() {
+        SettingsManager settings = SettingsManager.getInstance();
+        swipeListView.setSwipeMode(settings.getSwipeMode());
+        swipeListView.setSwipeActionLeft(settings.getSwipeActionLeft());
+        swipeListView.setSwipeActionRight(settings.getSwipeActionRight());
+        swipeListView.setOffsetLeft(convertDpToPixel(settings.getSwipeOffsetLeft()));
+        swipeListView.setOffsetRight(convertDpToPixel(settings.getSwipeOffsetRight()));
+        swipeListView.setAnimationTime(settings.getSwipeAnimationTime());
+        swipeListView.setSwipeOpenOnLongPress(settings.isSwipeOpenOnLongPress());
+    }
+
+    public int convertDpToPixel(float dp) {
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        float px = dp * (metrics.densityDpi / 160f);
+        return (int) px;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_app, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        boolean handled = false;
+        switch (item.getItemId()) {
+            case android.R.id.home: //Actionbar home/up icon
+                finish();
+                break;
+            case R.id.menu_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_SETTINGS);
+                break;
+        }
+        return handled;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CODE_SETTINGS:
+                reload();
+        }
+    }
+
+    public class ListAppTask extends AsyncTask<Void, Void, List<PackageItem>> {
+
+        protected List<PackageItem> doInBackground(Void... args) {
+            PackageManager appInfo = getPackageManager();
+            List<ApplicationInfo> listInfo = appInfo.getInstalledApplications(0);
+            Collections.sort(listInfo, new ApplicationInfo.DisplayNameComparator(appInfo));
+
+            List<PackageItem> data = new ArrayList<PackageItem>();
+
+            for (int index = 0; index < listInfo.size(); index++) {
+                try {
+                    ApplicationInfo content = listInfo.get(index);
+                    if ((content.flags != ApplicationInfo.FLAG_SYSTEM) && content.enabled) {
+                        if (content.icon != 0) {
+                            PackageItem item = new PackageItem();
+                            item.setName(getPackageManager().getApplicationLabel(content).toString());
+                            item.setPackageName(content.packageName);
+                            item.setIcon(getPackageManager().getDrawable(content.packageName, content.icon, content));
+                            data.add(item);
+                        }
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+
+            return data;
+        }
+
+        protected void onPostExecute(List<PackageItem> result) {
+            data.clear();
+            data.addAll(result);
+            adapter.notifyDataSetChanged();
+            if (progressDialog != null) {
+                progressDialog.dismiss();
+                progressDialog = null;
+            }
+            if (PreferencesManager.getInstance(SwipeListViewExampleActivity.this).getShowAbout()) {
+                AboutDialog logOutDialog = new AboutDialog();
+                logOutDialog.show(getSupportFragmentManager(), "dialog");
+            }
+        }
+    }
+
+}
